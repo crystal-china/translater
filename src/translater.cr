ae7e4817edc65946a544d9292287dbc41c838f0a
Billy.Zheng <vil963@gmail.com>
2023-02-03 01:52:40 +0800

    Try fix `Session is already started` issue

--- src/translater.cr
+++ src/translater.cr
@@ -3,59 +3,78 @@ require "selenium"
 require "./translater/**"
 
 class Translater
+  def create_session(browser, debug_mode)
+    case browser
+    in Browser::Firefox
+      if Webdrivers::Geckodriver.driver_version
+        driver_path = Webdrivers::Geckodriver.driver_path
+      else
+        driver_path = Webdrivers::Geckodriver.install
+      end
+
+      service = Selenium::Service.firefox(driver_path: File.expand_path(driver_path, home: true))
+      driver = Selenium::Driver.for(:firefox, service: service)
+      options = Selenium::Firefox::Capabilities::FirefoxOptions.new
+      options.args = ["--headless"] unless debug_mode == true
+
+      capabilities = Selenium::Firefox::Capabilities.new
+      capabilities.firefox_options = options
+      # when "chrome"
+      #   if Webdrivers::Chromedriver.driver_version
+      #     driver_path = Webdrivers::Chromedriver.driver_path
+      #   else
+      #     driver_path = Webdrivers::Chromedriver.install
+      #   end
+
+      #   service = Selenium::Service.chrome(driver_path: File.expand_path(driver_path, home: true))
+      #   driver = Selenium::Driver.for(:chrome, service: service)
+
+      #   options = Selenium::Chrome::Capabilities::ChromeOptions.new
+      #   options.args = ["headless"] unless debug_mode == true
+
+      #   capabilities = Selenium::Chrome::Capabilities.new
+      #   capabilities.chrome_options = options
+      # else
+      #   STDERR.puts "Only support firefox for now, firefox is default."
+      #   exit
+    end
+
+    if driver.status.ready?
+      session = driver.create_session(capabilities)
+    else
+      session = nil
+    end
+
+    {driver, session}
+  end
+
   def initialize(content, target_language, debug_mode, browser, engine_list, timeout_seconds)
     return if content == "--help"
 
+    driver, session = create_session(browser, debug_mode)
+
+    while session.nil?
+      STDERR.puts "Try restart current geckodriver ..."
+      system("pkill geckodriver")
+      sleep 1
+      driver, session = create_session(browser, debug_mode)
+    end
+
+    new_session = session.not_nil!
+
     begin
-      case browser
-      in Browser::Firefox
-        if Webdrivers::Geckodriver.driver_version
-          driver_path = Webdrivers::Geckodriver.driver_path
-        else
-          driver_path = Webdrivers::Geckodriver.install
-        end
-
-        service = Selenium::Service.firefox(driver_path: File.expand_path(driver_path, home: true))
-        driver = Selenium::Driver.for(:firefox, service: service)
-        options = Selenium::Firefox::Capabilities::FirefoxOptions.new
-        options.args = ["--headless"] unless debug_mode == true
-
-        capabilities = Selenium::Firefox::Capabilities.new
-        capabilities.firefox_options = options
-        # when "chrome"
-        #   if Webdrivers::Chromedriver.driver_version
-        #     driver_path = Webdrivers::Chromedriver.driver_path
-        #   else
-        #     driver_path = Webdrivers::Chromedriver.install
-        #   end
-
-        #   service = Selenium::Service.chrome(driver_path: File.expand_path(driver_path, home: true))
-        #   driver = Selenium::Driver.for(:chrome, service: service)
-
-        #   options = Selenium::Chrome::Capabilities::ChromeOptions.new
-        #   options.args = ["headless"] unless debug_mode == true
-
-        #   capabilities = Selenium::Chrome::Capabilities.new
-        #   capabilities.chrome_options = options
-        # else
-        #   STDERR.puts "Only support firefox for now, firefox is default."
-        #   exit
-      end
-
-      session = driver.create_session(capabilities)
-
       # Clean Cookies
-      cookie_manager = Selenium::CookieManager.new(command_handler: session.command_handler, session_id: session.id)
+      cookie_manager = Selenium::CookieManager.new(command_handler: new_session.command_handler, session_id: session.id)
       cookie_manager.delete_all_cookies
 
       chan = Channel(Nil).new
 
       spawn do
         begin
-          Youdao.new(session, content, debug_mode) if engine_list.includes? Engine::Youdao
-          Tencent.new(session, content, debug_mode) if engine_list.includes? Engine::Tencent
-          Ali.new(session, content, debug_mode) if engine_list.includes? Engine::Ali
-          Baidu.new(session, content, debug_mode) if engine_list.includes? Engine::Baidu
+          Youdao.new(new_session, content, debug_mode) if engine_list.includes? Engine::Youdao
+          Tencent.new(new_session, content, debug_mode) if engine_list.includes? Engine::Tencent
+          Ali.new(new_session, content, debug_mode) if engine_list.includes? Engine::Ali
+          Baidu.new(new_session, content, debug_mode) if engine_list.includes? Engine::Baidu
 
           chan.send(nil)
         rescue e : Selenium::Error
@@ -69,10 +88,10 @@ class Translater
       when timeout (engine_list.size * timeout_seconds).seconds
         STDERR.puts %{Timeout! engine: #{engine_list.join(", ")}}
       end
+    ensure
+      new_session.delete
+      driver.stop
     end
-  ensure
-    session.delete unless session.nil?
-    driver.stop unless driver.nil?
   end
 
   def self.input(element, content)
