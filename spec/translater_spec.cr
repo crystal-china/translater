ef8ffacde070b3c2b83aa2015a509a5975502340
Billy.Zheng <vil963@gmail.com>
2023-05-12 00:12:22 +0800

    Refactor CI

--- spec/translater_spec.cr
+++ spec/translater_spec.cr
@@ -3,38 +3,39 @@ require "./spec_helper"
 describe "Translater" do
   before_all do
     system("shards build")
+    system("./bin/translater --profile")
   end
 
-  it "no args should work", tags: "cii" do
+  it "no args should work", tags: "ci" do
     system("./bin/translater").should be_true
   end
 
   it "translate C/E use default engine" do
-    system("./bin/translater --no-profile '你好，中国！'").should be_true
+    system("./bin/translater '你好，中国！'").should be_true
   end
 
   it "translate E/C use default engine" do
-    system("./bin/translater --no-profile 'Hello, China!'").should be_true
+    system("./bin/translater 'Hello, China!'").should be_true
   end
 
   it "translate C/E use youdao", tags: "ci" do
-    system("./bin/translater --no-profile -e youdao '你好，中国！'").should be_true
+    system("./bin/translater -e youdao '你好，中国！'").should be_true
   end
 
   it "translate E/C use youdao", tags: "ci" do
-    system("./bin/translater --no-profile -e youdao 'Hello, China!'").should be_true
+    system("./bin/translater -e youdao 'Hello, China!'").should be_true
   end
 
   it "translate E/C use youdao+baidu" do
-    system("./bin/translater --no-profile -e baidu,youdao 'Hello, China!'").should be_true
+    system("./bin/translater -e baidu,youdao 'Hello, China!'").should be_true
   end
 
   it "translate E/C use all supported engines" do
-    system("./bin/translater --no-profile -A 'Hello, China!'").should be_true
+    system("./bin/translater -A 'Hello, China!'").should be_true
   end
 
   it "should translate multi-line english" do
-    system(%{./bin/translater --no-profile "Specify target language, support zh-CN|en for now.
+    system(%{./bin/translater "Specify target language, support zh-CN|en for now.
   default is translate English to Chinese.
   Youdao don't support this option."}).should be_true
   end
