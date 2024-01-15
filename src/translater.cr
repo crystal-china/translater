76736cce151d1c0a2e114d0528fea9a2aa764dcd
Billy.Zheng <vil963@gmail.com>
2024-01-15 17:14:00 +0800

    Try not cleanup cookies between run

--- src/translater.cr
+++ src/translater.cr
@@ -63,9 +63,9 @@ If it still doesn't work, try delete files under ~/.webrivers and try again."
   def create_session(driver, capabilities)
     new_session = driver.create_session(capabilities)
 
-    # Clean Cookies
-    cookie_manager = Selenium::CookieManager.new(command_handler: new_session.command_handler, session_id: new_session.id)
-    cookie_manager.delete_all_cookies
+    # # Clean Cookies
+    # cookie_manager = Selenium::CookieManager.new(command_handler: new_session.command_handler, session_id: new_session.id)
+    # cookie_manager.delete_all_cookies
 
     new_session
   end
